015124683b784ed882795cf65e30af32cc306fae
1Ramez <Ramezmohamed93@gmail.com>
2025-11-12 11:43:34 +0200

    Added the games menu

---
+++ Menu.cpp
@@ -0,0 +1,34 @@
+#include <iostream> // Required for input/output operations (cout, cin)
+#include <string>   // Required for string
+#include <vector>   // Required for vector
+#include <memory>   // Required for unique_ptr
+
+#include "BoardGame_Classes.h"
+using namespace std;
+
+int main(){
+    string choice;
+    while(true){
+        cout << "For Numerical Tic-Tac-Toe Enter 1\n";
+        cout << "Enter your choice: ";
+        cin >> choice;
+        if (choice == "1"){
+            //Numerical Tic-Tac-Toe variables and calls here
+
+
+        }else{
+            cout << "Invalid Choice, Please select a valid number";
+        }
+        
+        //Replaying the games menu
+        string again;
+        cout << "If you want to play again Enter 1\nIf you want to close the app Enter anything\n";
+        cout << "Enter your choice: ";
+        cin >> again;
+        if(again != "1"){
+            break;
+        }
+    }
+    
+    return 0;
+}
