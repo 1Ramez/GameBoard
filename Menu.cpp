154c0b00275333ca4d7b32a24ecfb47a632a04a3
1Ramez <Ramezmohamed93@gmail.com>
2025-11-12 12:07:27 +0200

    Fixed the Menu

--- Menu.cpp
+++ Menu.cpp
@@ -9,24 +9,27 @@ using namespace std;
 int main(){
     string choice;
     while(true){
+        bool playing = false;
         cout << "For Numerical Tic-Tac-Toe Enter 1\n";
         cout << "Enter your choice: ";
         cin >> choice;
         if (choice == "1"){
             //Numerical Tic-Tac-Toe variables and calls here
 
-
+            playing = true;
         }else{
             cout << "Invalid Choice, Please select a valid number";
         }
         
         //Replaying the games menu
-        string again;
-        cout << "If you want to play again Enter 1\nIf you want to close the app Enter anything\n";
-        cout << "Enter your choice: ";
-        cin >> again;
-        if(again != "1"){
-            break;
+        if (playing){
+            string again;
+            cout << "If you want to play again Enter 1\nIf you want to close the app Enter anything\n";
+            cout << "Enter your choice: ";
+            cin >> again;
+            if(again != "1"){
+                break;
+            }
         }
     }
     
