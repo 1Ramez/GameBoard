5755601305c40f2af59d2b4f76930b813a0d1c46
MoElsarawry <mohamedeslamelsarawy@gmail.com>
2025-11-26 20:14:16 +0200

    Made some edits

--- Misere.cpp
+++ Misere.cpp
@@ -4,44 +4,42 @@ using namespace std;
 
 Misere_Board::Misere_Board() : Board<char>(3,3) {
     for (auto& row : board)
-        for (char &c : row)
-            c = ' ';
+        for (auto& cell : row)
+            cell = ' ';
 }
 
 bool Misere_Board::update_board(Move<char>* move) {
     int x = move->get_x();
     int y = move->get_y();
-    char s = move->get_symbol();
+    char Sym = move->get_symbol();
 
     if (x < 0 || x >= 3 || y < 0 || y >= 3)
         return false;
     if (board[x][y] != ' ')
         return false;
 
-    board[x][y] = s;
+    board[x][y] = Sym;
     n_moves++;
     return true;
 }
 
-// Check normal 3-in-row (used for lose condition)
 bool Misere_Board::is_lose(Player<char>* player) {
-    char s = player->get_symbol();
+    char Sym = player->get_symbol();
 
     for (int i = 0; i < 3; i++) {
-        if (board[i][0] == s && board[i][1] == s && board[i][2] == s)
+        if (board[i][0] == Sym && board[i][1] == Sym && board[i][2] == Sym)
             return true;
-        if (board[0][i] == s && board[1][i] == s && board[2][i] == s)
+        if (board[0][i] == Sym && board[1][i] == Sym && board[2][i] == Sym)
             return true;
     }
-    if (board[0][0] == s && board[1][1] == s && board[2][2] == s)
+    if (board[0][0] == Sym && board[1][1] == Sym && board[2][2] == Sym)
         return true;
-    if (board[0][2] == s && board[1][1] == s && board[2][0] == s)
+    if (board[0][2] == Sym && board[1][1] == Sym && board[2][0] == Sym)
         return true;
 
     return false;
 }
 
-// Win = your opponent loses
 bool Misere_Board::is_win(Player<char>* player) {
     Player<char>* other = (player->get_symbol() == 'X')
         ? new Player<char>("tmp",'O',PlayerType::HUMAN)
@@ -60,10 +58,8 @@ bool Misere_Board::game_is_over(Player<char>* player) {
     return is_lose(player) || is_win(player) || is_draw(player);
 }
 
-/* UI */
-
 Misere_UI::Misere_UI()
-    : UI<char>("Welcome to Misere (Reverse) Tic-Tac-Toe!", 3) {}
+    : UI<char>("Welcome to FCAI Misere (Reverse) Tic-Tac-Toe!", 3) {}
 
 Player<char>* Misere_UI::create_player(string& name, char symbol, PlayerType type) {
     return new Player<char>(name, symbol, type);
@@ -74,7 +70,7 @@ Move<char>* Misere_UI::get_move(Player<char>* player) {
     int x, y;
 
     if (player->get_type() == PlayerType::HUMAN) {
-        cout << player->get_name() << " enter row and col: ";
+        cout << player->get_name() << " please enter row and col: ";
         cin >> x >> y;
 
         return new Move<char>(x, y, player->get_symbol());
--- Misere.h
+++ Misere.h
@@ -1,6 +1,5 @@
 #ifndef MISERE_H
 #define MISERE_H
-
 #include "BoardGame_Classes.h"
 #include <iostream>
 using namespace std;
@@ -9,8 +8,8 @@ class Misere_Board : public Board<char> {
 public:
     Misere_Board();
     bool update_board(Move<char>* move) override;
-    bool is_win(Player<char>* player) override;   // in misère, win = opponent loses
-    bool is_lose(Player<char>* player) override;  // player loses if THEY make 3-in-row
+    bool is_win(Player<char>* player) override;   
+    bool is_lose(Player<char>* player) override;  
     bool is_draw(Player<char>* player) override;
     bool game_is_over(Player<char>* player) override;
 };
